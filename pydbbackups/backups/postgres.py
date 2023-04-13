ba1b847501b7d56408e69ef71d3aa94a03eef97a
jbuendia1y <jgamer669@gmail.com>
2023-04-13 09:15:03 -0500

    #2 Resolved

--- pydbbackups/backups/postgres.py
+++ pydbbackups/backups/postgres.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 from .base import Backup
 import subprocess
 import gzip
@@ -7,7 +8,10 @@ from io import BytesIO
 
 class Postgres(Backup):
 
-    CMDS_TO_CHECK = [('pg_dump', '--version')]
+    CMDS_TO_CHECK = [
+        ('pg_dump', '--version'),
+        ('pg_restore', '--version'),
+        ('psql', '--version')]
 
     def dump(self) -> BytesIO:
         # host = f"-h {self.host}"
@@ -41,3 +45,37 @@ class Postgres(Backup):
             return BytesIO(compressed)
 
         return BytesIO(p.stdout.read())
+
+    def restore(self, file_path: Path) -> BytesIO:
+        env = os.environ.copy()
+        if self.password:
+            env['PGPASSWORD'] = self.password
+
+        args = [
+            '-h',
+            self.host,
+            '-p',
+            f"{self.port}",
+            '-U',
+            self.username,
+            '-d',
+            self.database,
+        ]
+
+        f_arg = [
+            '-f',
+            f"{file_path.absolute()}",
+        ] if str(file_path).endswith('.sql') else [
+            f"{file_path.absolute()}",
+        ]
+
+        p = subprocess.Popen([
+            'psql' if str(file_path).endswith('.sql') else 'pg_restore',
+            *args,
+            *f_arg
+        ], env=env, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+        code = p.wait()
+        if code > 0:
+            print(p.stderr)
+            raise Exception(p.stderr.read().decode('utf-8'))
